3d6d8d891e601ff55543e68e074f787d7da1fe3d
NilSousa16 <nil.sousa16@gmail.com>
2020-10-23 12:25:13 -0300

    Updated fake api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.100:3333',
+  baseURL: 'http://192.168.0.102:3333',
 });
+
+/*
+  https://stackoverflow.com/questions/51026532/json-server-cannot-access-via-local-ip
+  Rodar a fake api: yarn json-server -H 192.168.0.102 -p 3333 -w server.json
+*/
 
 export default api;
